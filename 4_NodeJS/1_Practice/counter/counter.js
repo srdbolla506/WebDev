f6ee4cd3fc0c88bb09613b782b206d533b078091
Sri Divya Bolla <sridivya.bolla@gmail.com>
2024-10-16 08:02:28 +0530

    Node js assignment to create cli for counter and todolist

---
+++ 4_NodeJS/1_Practice/counter/counter.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { Command } = require('commander');
+
+const program = new Command();
+
+
+program
+    .name('Words Counter')
+    .description('CLI to some JS file utilities')
+    .version('0.8.0');
+
+program.command('counter')
+    .description('Count the number of words in a file with filepath as input')
+    .argument('<string>', 'filepath string')
+    .option('--first', 'display just the first word in the file')
+    .option('-s, --separator <char>', 'separator character', ' ')
+    .action((filepath, options) => {
+        fs.readFile(filepath, 'utf-8', function(err, data) {
+            const words = data.split(options.separator);
+            console.log(words.length);
+        });
+    });
+
+program.parse();
